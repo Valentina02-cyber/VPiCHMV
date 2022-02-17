26b0c19175a0f0a748abd5a24015ab1f136a5be8
Валентина Дубинина <valjntik@mail.ru>
2022-02-17 12:38:03 +0700

    add lab1

---
+++ VisualLab1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace VisualLab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num1 = { 5, 3, 4 };
+            Console.WriteLine(HW1.QueueTime(num1, 1));
+            int[] num2 = { 10, 2, 3, 3 };
+            Console.WriteLine(HW1.QueueTime(num2, 2));
+            int[] num3 = { 2, 3, 10 };
+            Console.WriteLine(HW1.QueueTime(num3, 2));
+        }
+    }
+}
